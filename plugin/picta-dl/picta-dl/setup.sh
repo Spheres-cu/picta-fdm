80a61caaf0d1d383b04d0070edbd34a7eacb9317
Spheres_cu <72468068+Spheres-cu@users.noreply.github.com>
2026-08-31 14:41:08 -0400

    Fix mis plugin folder structure error

--- plugin/picta-dl/picta-dl/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Setup for PyInstaller ManyLinux 2.28 Docker Action
-# https://github.com/oleksis/pyinstaller-manylinux
-
-# Fail on errors.
-set -e
-
-# SSL: CERTIFICATE
-pyenv exec pip install -r requirements/requirements.txt
